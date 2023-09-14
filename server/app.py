4af11e1764b45d4fa00d0a8909d6db431be13424
abdhata <abdhata@outlook.com>
2023-09-14 07:47:59 +0300

    3 passed, 10 more to go

--- server/app.py
+++ server/app.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+# append to app/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<studentname>')
+def student(studentname):
+    print(f'This is the student details for {studentname}')
+    return f'<h1>Student details for {studentname}</h1>'
